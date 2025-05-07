99ed6de84df878d65ff4c567376762daa77d8bd0
me <cutefiles@rawiweb.de>
2025-05-07 15:45:48 +0200

    Version 1 of the cutefiles

--- maxbytes.php
+++ maxbytes.php
@@ -1,9 +1,11 @@
 <?php
 //sets some javascript variables needed for Uploads 
 $num1 = return_bytes(ini_get("post_max_size"));
+$numV = getsize(ini_get("post_max_size"));
 $num2 = return_bytes(ini_get("upload_max_filesize"));
 $min = ($num1 <=> $num2) < 0 ? $num1 : $num2;
 echo "const maxBytes = $min;". PHP_EOL;
+echo "const verodMaxBytes = $numV;". PHP_EOL;
 echo "const fileUploads = ".ini_get("file_uploads"). PHP_EOL;
 echo "const maxFileUploads = ".ini_get("max_file_uploads"). PHP_EOL;
 
@@ -28,3 +30,17 @@ function return_bytes($val) {
     }
     return $val;
 }
+    function getsize($size) {
+        if ($size === null) return null;
+        $last = is_string($size) ? strtolower(substr($size, -1)) : null;
+        $size = (int) $size;
+        switch($last) {
+            case 'g':
+                $size *= 1024;
+            case 'm':
+                $size *= 1024;
+            case 'k':
+                $size *= 1024;
+        }
+        return $size;
+    }
